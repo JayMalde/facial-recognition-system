929aa6cc1a8182ffbd8a66f22b7b16e0ed336645
JayMalde <53506120+JayMalde@users.noreply.github.com>
2020-10-11 13:13:27 +0530

    Update settings.py

--- face_recognition_system/settings.py
+++ face_recognition_system/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='maldejay7@gmail.com'
-EMAIL_HOST_PASSWORD='9004422995'
+EMAIL_HOST_USER='email id'
+EMAIL_HOST_PASSWORD='email password'
