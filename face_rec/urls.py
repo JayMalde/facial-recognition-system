59ed10607c30f3495e747a66835b76de937b7587
JayMalde <53506120+JayMalde@users.noreply.github.com>
2021-04-07 12:33:26 +0530

    Update urls.py

--- face_rec/urls.py
+++ face_rec/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
 	path('login/', views.loginPage, name="login"),   
 	path('logout/', views.logoutUser,name="logout"),
 
-	path('create_dataset/', views.create_dataset,name="create_dataset"),
-	path('trainer/', views.trainer,name="trainer"),
-	path('detect/', views.TrackImages,name="detect"),
+	path('create_dataset', views.create_dataset,name="create_dataset"),
+	path('trainer', views.trainer,name="trainer"),
+	path('detect', views.TrackImages,name="detect"),
 
 	path('profile/<str:pk>/', views.profile,name="profile"),
 	path('all_students/', views.all_students,name="all_students"),
